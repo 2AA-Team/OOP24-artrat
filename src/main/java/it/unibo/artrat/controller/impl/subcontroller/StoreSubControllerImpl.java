ef00918eb577321bb1f58fc11339b6f5048278df
Manuel Benagli <manuel.benagli@studio.unibo.it>
2025-02-14 17:00:40 +0100

    created getMarket and setMarket methods on Model interface, continuing StoreSunControllerImpl and MarketImpl

--- src/main/java/it/unibo/artrat/controller/impl/subcontroller/StoreSubControllerImpl.java
+++ src/main/java/it/unibo/artrat/controller/impl/subcontroller/StoreSubControllerImpl.java
@@ -4,7 +4,13 @@ import java.util.List;
 import it.unibo.artrat.controller.api.subcontroller.StoreSubController;
 import it.unibo.artrat.controller.impl.AbstractSubController;
 import it.unibo.artrat.controller.impl.MainControllerImpl;
+import it.unibo.artrat.model.api.Model;
+import it.unibo.artrat.model.api.characters.Player;
+import it.unibo.artrat.model.api.inventory.Inventory;
 import it.unibo.artrat.model.api.inventory.Item;
+import it.unibo.artrat.model.api.market.Market;
+import it.unibo.artrat.model.impl.ModelImpl;
+import it.unibo.artrat.model.impl.characters.PlayerImpl;
 import it.unibo.artrat.view.api.MarketView;
 import it.unibo.artrat.view.impl.MarketSubPanel;
 
@@ -26,15 +32,25 @@ public class StoreSubControllerImpl extends AbstractSubController implements Sto
 
     @Override
     public List<Item> purchasableItems() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'purchasableItems'");
+        return this.getModel().getMarket().getPurchItems();
     }
 
     @Override
     public boolean buyItem(Item itemToBuy) {
-        if(){
+        final Model model = this.getModel();
+        final Player player = model.getPlayer();
+        final Market market = this.getModel().getMarket();
+        final Inventory inv = player.getInventory();
+        if(market.buyItem(itemToBuy)){
+            inv.addItem(itemToBuy);
+            
 
+            final Player modPlayer = inv.addItem(itemToBuy);
+            model.setPlayer(new PlayerImpl());
+            this.updateCentralizeModel(new ModelImpl(model));
+            return true;
         }
+        return false;
     }
 
     @Override
--- src/main/java/it/unibo/artrat/model/impl/market/MarketImpl.java
+++ src/main/java/it/unibo/artrat/model/impl/market/MarketImpl.java
@@ -3,29 +3,38 @@ package it.unibo.artrat.model.impl.market;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.unibo.artrat.model.api.characters.Player;
 import it.unibo.artrat.model.api.inventory.Item;
+import it.unibo.artrat.model.api.inventory.ItemType;
 import it.unibo.artrat.model.api.market.Market;
 
 public class MarketImpl implements Market{    
     private final List<Item> itemsToBuy;
-
+    
     public MarketImpl(){
         this.itemsToBuy = new ArrayList<>();
     }
 
-    public MarketImpl(final Market itemsToBuy ){
+    public MarketImpl(final Market itemsToBuy){
         this.itemsToBuy = itemsToBuy.getPurchItems();
     }
 
     @Override
     public List<Item> getPurchItems(){
-        
-        throw new UnsupportedOperationException("Unimplemented method 'getPurchItems'");
+        return new ArrayList<>(itemsToBuy);
     }
 
     @Override
     public boolean buyItem(Item passedItem){
+        final Player player = ;
 
-        throw new UnsupportedOperationException("Unimplemented method 'buyItem'");
+        if(itemsToBuy.contains(passedItem) && player.getCoin().getCurrentAmount() >= passedItem.getPrice()){
+            player.getCoin().spendCoins(passedItem.getPrice());
+            if(passedItem.getType() == ItemType.POWERUP){
+                itemsToBuy.remove(passedItem);
+            }
+            return true;
+        }
+        return false;
     }
 }
