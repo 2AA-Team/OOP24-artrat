7b11d4b233d182389870aa6231c96e1a25ee6767
Matteo Tonelli <matteo.tonelli8@studio.unibo.it>
2025-02-19 20:50:45 +0100

    corrected some checkstyle

--- src/main/java/it/unibo/artrat/model/impl/world/roomgeneration/RoomGenerationMaze.java
+++ src/main/java/it/unibo/artrat/model/impl/world/roomgeneration/RoomGenerationMaze.java
@@ -1,5 +1,6 @@
 package it.unibo.artrat.model.impl.world.roomgeneration;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -13,65 +14,65 @@ import it.unibo.artrat.model.impl.AbstractGameObject;
 import it.unibo.artrat.model.impl.GameObjectFactoryImpl;
 import it.unibo.artrat.utils.impl.Point;
 
+/**
+ * room generation based on DFS with backtracking.
+ */
 public class RoomGenerationMaze implements RoomGenerationStrategy {
 
     private final GameObjectFactory factory = new GameObjectFactoryImpl();
     private static final Random RANDOM = new Random();
-    private Set<AbstractGameObject> maze;
+    private Set<AbstractGameObject> maze = new HashSet<>();
 
     /**
      * {@inheritDoc}
      */
     @Override
     public Set<AbstractGameObject> generateRoomSet(final int size) {
-        Set<AbstractGameObject> border = new RoomGenerationEmpty().generateRoomSet(size);
-        // Converte lo stream in un Set per riutilizzarlo più volte
-        Set<AbstractGameObject> allFilledSet = fullFilled(size).collect(Collectors.toSet());
-        // Filtra per creare il labirinto (muri esterni)
+        final Set<AbstractGameObject> border = new RoomGenerationEmpty().generateRoomSet(size);
+        final Set<AbstractGameObject> allFilledSet = fullFilled(size).collect(Collectors.toSet());
         maze = allFilledSet.stream()
                 .filter(x -> x.getPosition().getX() % 2 == 0 || x.getPosition().getY() % 2 == 0)
                 .collect(Collectors.toSet());
-
-        List<Point> visited = allFilledSet.stream()
+        final List<Point> visited = allFilledSet.stream()
                 .filter(x -> !maze.contains(x))
                 .map(AbstractGameObject::getPosition)
                 .collect(Collectors.toList());
-
         if (!visited.isEmpty()) {
-            Point currentPoint = visited.get(RANDOM.nextInt(visited.size()));
+            final Point currentPoint = visited.get(RANDOM.nextInt(visited.size()));
             createMaze(visited, currentPoint);
         }
         return Stream.concat(maze.stream(), border.stream()).collect(Collectors.toSet());
     }
 
-    private void createMaze(List<Point> visited, Point currentPoint) {
+    private void createMaze(final List<Point> visited, final Point currentPoint) {
         if (visited.isEmpty()) {
             return;
         }
-        if (currentPoint == null) {
-            currentPoint = visited.remove(RANDOM.nextInt(visited.size()));
+        Point currentPointcopy = currentPoint;
+        if (currentPointcopy == null) {
+            currentPointcopy = visited.remove(RANDOM.nextInt(visited.size()));
         }
-        List<Point> stack = new java.util.ArrayList<>();
-        stack.add(currentPoint);
+        final List<Point> stack = new java.util.ArrayList<>();
+        stack.add(currentPointcopy);
         while (!stack.isEmpty()) {
-            Point tmp = stack.get(stack.size() - 1);
-            List<Point> neighbours = visited.stream()
+            final Point tmp = stack.get(stack.size() - 1);
+            final List<Point> neighbours = visited.stream()
                     .filter(p -> p.getDistance(tmp) == 2)
                     .collect(Collectors.toList());
             if (neighbours.isEmpty()) {
                 stack.remove(stack.size() - 1);
             } else {
-                Point next = neighbours.get(RANDOM.nextInt(neighbours.size()));
+                final Point next = neighbours.get(RANDOM.nextInt(neighbours.size()));
                 visited.remove(next);
-                int midX = (int) (next.getX() + tmp.getX()) / 2;
-                int midY = (int) (next.getY() + tmp.getY()) / 2;
+                final int midX = (int) (next.getX() + tmp.getX()) / 2;
+                final int midY = (int) (next.getY() + tmp.getY()) / 2;
                 maze.removeIf(x -> x.getPosition().equals(new Point(midX, midY)));
                 stack.add(next);
             }
         }
     }
 
-    private Stream<AbstractGameObject> fullFilled(int size) {
+    private Stream<AbstractGameObject> fullFilled(final int size) {
         return IntStream.range(1, size - 1)
                 .boxed()
                 .flatMap(i -> IntStream.range(1, size - 1)
