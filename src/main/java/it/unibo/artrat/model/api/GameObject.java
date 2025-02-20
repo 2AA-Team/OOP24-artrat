8be2c01281fe8ccc5b82de27f6f73877b0b583fa
Samuele Trapani <samuele.trapani@studio.unibo.it>
2025-02-20 15:50:15 +0100

    player collision detection not working

--- src/main/java/it/unibo/artrat/model/api/GameObject.java
+++ src/main/java/it/unibo/artrat/model/api/GameObject.java
@@ -1,5 +1,6 @@
 package it.unibo.artrat.model.api;
 
+import it.unibo.artrat.utils.api.BoundingBox;
 import it.unibo.artrat.utils.impl.Point;
 
 /**
@@ -27,5 +28,13 @@ public interface GameObject {
      * @param p new position
      */
     void setPosition(Point p);
+
+    /**
+     * Collision check with other bounding box.
+     * 
+     * @param box
+     * @return true if colliding, false otherwise
+     */
+    BoundingBox getBoundingBox();
 
 }
