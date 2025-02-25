bca4aabcc715331493f0ac5e87412744c8524e0b
Matteo Tonelli <matteo.tonelli8@studio.unibo.it>
2025-02-25 11:03:45 +0100

    fixed some checkstyle

--- src/main/java/it/unibo/artrat/model/impl/missioncenter/missions/RatRace.java
+++ src/main/java/it/unibo/artrat/model/impl/missioncenter/missions/RatRace.java
@@ -8,10 +8,11 @@ import it.unibo.artrat.model.impl.missioncenter.AbstractMission;
  */
 public class RatRace extends AbstractMission {
     private static final double MAX_SPEED = 0.02;
+    private static final double EPSILON = 1e-6;
 
     /**
-     * @param name mission's name.
-     * @param desc mission's description.
+     * @param name   mission's name.
+     * @param desc   mission's description.
      * @param status mission's status.
      */
     public RatRace(final String name, final String desc, final boolean status) {
@@ -23,9 +24,9 @@ public class RatRace extends AbstractMission {
      */
     @Override
     public boolean isMissionDone(final Player passedPlayer) {
-        if (!this.isStatusDone() && passedPlayer.getVelocity() == MAX_SPEED) {
-                this.setStatus(true);
-            }
+        if (!this.isStatusDone() && (Math.abs(MAX_SPEED - passedPlayer.getVelocity()) < EPSILON)) {
+            this.setStatus(true);
+        }
         return isStatusDone();
     }
 }
