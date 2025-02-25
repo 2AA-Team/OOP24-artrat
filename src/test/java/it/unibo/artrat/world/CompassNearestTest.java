e02958f86793240dafb0b26233a698986823c2a0
Matteo Tonelli <matteo.tonelli8@studio.unibo.it>
2025-02-25 18:51:58 +0100

    removed checkstyle

--- src/test/java/it/unibo/artrat/world/CompassNearestTest.java
+++ src/test/java/it/unibo/artrat/world/CompassNearestTest.java
@@ -2,12 +2,12 @@ package it.unibo.artrat.world;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.artrat.model.impl.CompassNearest;
@@ -15,40 +15,39 @@ import it.unibo.artrat.utils.impl.Point;
 
 class CompassNearestTest {
 
-    private CompassNearest compass;
-    private Supplier<Point> center;
-    private Supplier<List<Point>> north;
-
-    @BeforeEach
-    void setUp() {
-        center = () -> new Point(0, 0);
-
-    }
-
     @Test
     void testCompassFullList() {
-        north = () -> Arrays.asList(
+        final double delta = 0.001;
+        final double expected = 0.785;
+        CompassNearest compass;
+        Supplier<List<Point>> north = () -> Arrays.asList(
                 new Point(1, 1),
                 new Point(2, 2),
                 new Point(-1, -1),
                 new Point(0, 3));
+        final Supplier<Point> center = () -> new Point();
         compass = new CompassNearest(center, north);
-        assertEquals(0.785, compass.calculateAngle(), 0.001); // angle to (1,1)
+        assertEquals(expected, compass.calculateAngle(), delta); // angle to (1,1)
         north = () -> Arrays.asList(
                 new Point(-1, -1),
                 new Point(2, 2),
                 new Point(0, 3));
         compass = new CompassNearest(center, north);
-        assertNotEquals(0.785, compass.calculateAngle(), 0.001); // angle to (-1,-1)
+        assertNotEquals(expected, compass.calculateAngle(), delta); // angle to (-1,-1)
 
     }
 
     @Test
     void testGetEmptySupplier() {
-        north = () -> Arrays.asList();
+        final CompassNearest compass;
+        final Supplier<List<Point>> north = Arrays::asList;
+        Supplier<Point> center = () -> new Point();
         compass = new CompassNearest(center, north);
         assertEquals(0, compass.calculateAngle());
         center = () -> null;
-        assertEquals(0, compass.calculateAngle());
+        final CompassNearest compass2 = new CompassNearest(center, north);
+        assertThrows(NullPointerException.class, () -> {
+            compass2.calculateAngle();
+        });
     }
 }
