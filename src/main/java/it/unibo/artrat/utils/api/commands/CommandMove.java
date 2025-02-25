13b8dd13be808d61a6bfa899d5f2cef84477ef49
Samuele Trapani <samuele.trapani@studio.unibo.it>
2025-02-25 12:42:59 +0100

    Tagged commands and slightly changed execute methods

---
+++ src/main/java/it/unibo/artrat/utils/api/commands/CommandMove.java
@@ -0,0 +1,5 @@
+package it.unibo.artrat.utils.api.commands;
+
+public interface CommandMove extends Command {
+
+}
--- src/main/java/it/unibo/artrat/utils/impl/commands/StopLeft.java
+++ src/main/java/it/unibo/artrat/utils/impl/commands/StopLeft.java
@@ -1,20 +1,20 @@
 package it.unibo.artrat.utils.impl.commands;
 
 import it.unibo.artrat.model.api.characters.Player;
-import it.unibo.artrat.utils.api.commands.Command;
-import it.unibo.artrat.utils.impl.Vector2d;
+import it.unibo.artrat.utils.api.Directions;
+import it.unibo.artrat.utils.api.commands.CommandStop;
 
 /**
  * Stop move left command for entity movement.
  */
-public class StopMovingLeft implements Command {
+public class StopLeft implements CommandStop {
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void execute(final Player p) {
-        p.removeDirection(new Vector2d(-1, 0));
+        p.removeDirection(Directions.LEFT.vector());
     }
 
 }
